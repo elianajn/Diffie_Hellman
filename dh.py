07fb936c356e8fecd2eddc462b46dc07097b186c
Eliana Neurohr <elianajneu@gmail.com>
2019-12-16 20:59:48 -0700

    sophie germain

--- dh.py
+++ dh.py
@@ -1,7 +1,16 @@
+import math
+import decimal
+
 def compute_shared(g, p, k):
 	return (g ** k ) % p
 
-p = 23
+def sophie_germain(p,n):
+	for i in range(n):
+		p = (2*p) + 1
+	return p
+p = sophie_germain(23,50) #computes a large prime number
+# p = decimal.Decimal(2**1536 - 2**1472 - 1 + 2**64 * { decimal.Decimal[2**(1406*math.pi)] + 741804 })
+print(p)
 g = 5 # A primitive root of 23
 a = 4
 b = 3
